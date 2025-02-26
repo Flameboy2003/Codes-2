d48f23562c32e9e7245253498870c19058841b1b
Akilan M <125997276+Flameboy2003@users.noreply.github.com>
2025-02-26 20:09:19 +0530

    Update O4_Palindrome.java

--- O4_Palindrome.java
+++ O4_Palindrome.java
@@ -2,11 +2,25 @@ package ABCTech;
 
 public class O4_Palindrome {
     public static void main(String[] args) {
-        String s = "Akilan";
-        int size = s.length();
-        char x[] = s.toCharArray();
-        char y[] = new char[x.length];
+         String s = "liril";
 
+        char x[] = s.toCharArray();
+        int size = x.length;
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < size / 2; i++) {
+            if (x[i] != x[size - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
 
     }
 }
